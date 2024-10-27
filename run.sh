ade9937a6dca933056b22b1a7037a35ce9aadef0
kp3073 <keyurpatel8288@gmail.com>
2024-10-27 09:39:03 -0400

    new11

--- run.sh
+++ run.sh
@@ -1,21 +1,6 @@
-#git clone https://github.com/kp3073/${COMPONENT}
-#cd $COMPONENT/schema
-#mysql -h${DBHOST} -u${DBUSER} -p${DBPASS} <${COMPONENT}.sql
-
-source /data/secrets
-git clone https://github.com/raghudevopsb78/${PROJECT_NAME}-${COMPONENT}
-cd ${PROJECT_NAME}-$COMPONENT
-
-if [ "${db_type}" == 'mongo' ]; then
-  curl -L -O https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
-  mongo --tls --host $DB_HOST:27017 --tlsCAFile global-bundle.pem --username ${DB_USER} --password $DB_PASS <schema/${COMPONENT}.js
-fi
-
-if [ "${db_type}" == 'mysql' ]; then
-  echo 'show databases;' | mysql -h${DB_HOST} -u${RDS_USER} -p${RDS_PASS} | grep cities
-  if [ $? -eq 0 ]; then exit ; fi
-  mysql -h${DB_HOST} -u${RDS_USER} -p${RDS_PASS} <schema/${COMPONENT}.sql
-fi
+git clone https://github.com/kp3073/${COMPONENT}
+cd $COMPONENT/schema
+mysql -h${DBHOST} -u${DBUSER} -p${DBPASS} <${COMPONENT}.sql
 
 
 
