1b0a9ee0d5b2f2e7feee338c82e0b342b56a43ac
kp3073 <keyurpatel8288@gmail.com>
2024-10-29 10:17:06 -0400

    new11

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/kp3073/${COMPONENT}
 cd $COMPONENT/schema
-mysql -h${DBHOST} -u${DBUSER} -p${DBPASS} <${COMPONENT}.sql
+mysql -h${DB_HOST} -u${DBUSER} -p${DBPASS} <${COMPONENT}.sql
 
 
 
